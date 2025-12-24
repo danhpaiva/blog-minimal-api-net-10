bea4236b2d62174ce5567b1c217b7638ede21321
Paiva <danpaiva@live.com>
2025-12-24 15:40:06 -0300

    feat: add controller home para o health-check

---
+++ Blog/Blog/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get() => Ok("Blog API is running...");
+}
