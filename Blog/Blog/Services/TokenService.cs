efcc8d0de86dcf6e4182f69e0cb2e5a5c06d0465
Paiva <danpaiva@live.com>
2025-12-24 18:19:29 -0300

    feat: add pacote Microsoft.AspNetCore.Authentication

---
+++ Blog/Blog/Services/TokenService.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Models;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        // Implementation for generating a token based on userId
+        return $"token_for_{userId}";
+    }
+}
