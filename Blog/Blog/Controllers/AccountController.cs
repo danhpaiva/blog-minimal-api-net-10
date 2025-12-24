481f1b596b155e62ebb3dc0b35d30efcadcbbe32
Paiva <danpaiva@live.com>
2025-12-24 19:24:38 -0300

    feat: remover metodos de teste

--- Blog/Blog/Controllers/AccountController.cs
+++ Blog/Blog/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
 ﻿using Blog.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
@@ -13,16 +12,4 @@ public class AccountController : ControllerBase
         var token = tokenService.GenerateToken(null);
         return Ok(token);
     }
-
-    [Authorize(Roles = "user")]
-    [HttpGet("v1/user")]
-    public IActionResult GetUser() => Ok(User.Identity.Name);
-
-    [Authorize(Roles = "author")]
-    [HttpGet("v1/author")]
-    public IActionResult GetAuthor() => Ok(User.Identity.Name);
-
-    [Authorize(Roles = "admin")]
-    [HttpGet("v1/admin")]
-    public IActionResult GetAdmin() => Ok(User.Identity.Name);
 }
