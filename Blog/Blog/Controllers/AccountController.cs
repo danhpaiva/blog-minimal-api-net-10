726c23d7fa85a28dbd5d4ea8492ae4666bcc2c31
Paiva <danpaiva@live.com>
2025-12-24 20:27:36 -0300

    feat: add metodo para criar novo usuario

--- Blog/Blog/Controllers/AccountController.cs
+++ Blog/Blog/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Blog.Extensions;
 using Blog.Services;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers;
 
@@ -10,7 +12,7 @@ namespace Blog.Controllers;
 public class AccountController : ControllerBase
 {
     [HttpPost("v1/accounts/")]
-    public async IActionResult Post(
+    public async Task<IActionResult> PostAsync(
         [FromBody] RegisterViewModel registerViewModel,
         [FromServices] AppDbContext context)
     {
@@ -24,7 +26,28 @@ public class AccountController : ControllerBase
             Slug = registerViewModel.Email.Replace("@", "-").Replace(".", "-")
         };
 
-        return null;
+        var password = PasswordGenerator.Generate(25);
+        user.PasswordHash = PasswordHasher.Hash(password);
+
+        try
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                user = user.Email,
+                password
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("06XE01 - Nao foi possivel criar o usuario com este email."));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<string>("06XE02 - Falha interna no servidor"));
+        }
     }
 
     [HttpPost("v1/accounts/login")]
