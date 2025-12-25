72c9e77c6a829639fdf513afdcffe6392bfc8468
Paiva <danpaiva@live.com>
2025-12-25 12:10:41 -0300

    feat: criar metodo de detalhes

--- Blog/Blog/Controllers/PostController.cs
+++ Blog/Blog/Controllers/PostController.cs
@@ -48,4 +48,39 @@ public class PostController : ControllerBase
             return StatusCode(500, new ResultViewModel<string>("09XE01 - Falha interna no servidor"));
         }
     }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> DetailsAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+        try
+        {
+            var post = await context
+           .Posts
+           .AsNoTracking()
+           .Include(p => p.Author)
+           .ThenInclude(p => p.Roles) //Evitar subselects
+           .Include(p => p.Category)
+              .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+                return NotFound(new ResultViewModel<string>("09XE02 - Conteudo nao encontrado"));
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                post.Id,
+                post.Title,
+                post.Slug,
+                post.Body,
+                post.LastUpdateDate,
+                Author = post.Author.Name,
+                Categories = post.Category.Name
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("09XE03 - Falha interna no servidor"));
+        }
+    }
 }
