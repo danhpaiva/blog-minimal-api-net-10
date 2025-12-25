1425e9d39f46d4c45b2b3a84715ee5a3866cd5a4
Paiva <danpaiva@live.com>
2025-12-25 12:17:19 -0300

    feat: injetar cache no app

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -62,6 +62,7 @@ void ConfigureAuthentication(WebApplicationBuilder builder)
 
 void ConfigureMvc(WebApplicationBuilder builder)
 {
+    builder.Services.AddMemoryCache();
     builder
     .Services
     .AddControllers()
