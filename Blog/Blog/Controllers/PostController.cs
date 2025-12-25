7bf24e3c895000aef7cf8aed8d49a77057db54db
Paiva <danpaiva@live.com>
2025-12-25 11:44:32 -0300

    feat: criar PostsController

---
+++ Blog/Blog/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class PostController : ControllerBase
+{
+    [HttpGet("v1/posts/")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] AppDbContext context)
+    {
+        return Ok(await context.Posts.ToListAsync());
+    }
+}
